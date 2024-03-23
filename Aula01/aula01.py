c0f2050a25f968388ee80ae6dd099f3ead992e5a
JOÃO PAULO BARROS PESSOA DE CARVALHO <108131013+JooJPaulo@users.noreply.github.com>
2024-03-22 21:19:58 -0300

    Add files via upload

---
+++ Aula01/aula01.py
@@ -0,0 +1,17 @@
+#Exercício 06
+'''
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+delta = b**2 - 4*a*c
+
+x1 = (-b + delta**(1/2))/(2*a)
+x2 = (-b - delta**(1/2))/(2*a)
+
+print("x1 = ", x1)
+print("x2 = ", x2)
+'''
+
+#Exercício 07
+
