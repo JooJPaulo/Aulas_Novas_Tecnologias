b457628e37a3b131ecc18fec7caa3330e6140e87
JOÃO PAULO BARROS PESSOA DE CARVALHO <108131013+JooJPaulo@users.noreply.github.com>
2024-05-08 20:54:17 -0300

    Delete Banco directory

--- Banco/Cliente.py
+++
@@ -1,8 +0,0 @@
-class Cliente:
-    def __init__(self, nome, sobrenome, cpf):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.cpf = cpf
-
-    def dados_cliente(self):
-        return f"Nome: {self.nome} {self.sobrenome}, CPF: {self.cpf}"
